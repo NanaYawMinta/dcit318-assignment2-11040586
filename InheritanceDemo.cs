a0ff74f48e82f80522f69c87d626e93d6e2b83ef
NanaYawMinta <okatakyieminta8@gmail.com>
2025-08-09 13:45:20 +0000

    added inheritance file

---
+++ InheritanceDemo.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace InheritanceDemo
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+            genericAnimal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+
+            Console.ReadLine();
+        }
+    }
+}
